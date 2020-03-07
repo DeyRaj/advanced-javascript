b93d6bf0161ce1ca0004e3e5dda21cf51e2371eb
DeyRaj <feb17rajandey@gmail.com>
2020-03-07 13:51:45 -0800

    null vs undefined cleared for interview

---
+++ undefined-vs-null.js
@@ -0,0 +1,20 @@
+// let name;
+
+// function add (num1 , num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add (num1 , num2){
+//     console.log(num1 , num2)
+// }
+
+// const identity = {name:"Rajan", phone:13445};
+// console.log(identity.home);
+
+// let fun = undefined;
+// console.log(fun);
+
+
+let ages =[12, 13, 14];
+console.log(ages[11]);
