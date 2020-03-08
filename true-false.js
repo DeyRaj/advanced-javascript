1b6a852c8908a4df30bce950097ae4c3c57b5931
DeyRaj <feb17rajandey@gmail.com>
2020-03-08 14:28:52 -0700

    true vs false

---
+++ true-false.js
@@ -0,0 +1,31 @@
+// const age=0;
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+
+//falsy
+//0
+//false
+//""
+//null
+//NaN - Not a number
+//const name="";
+//undefined
+
+//Truthy
+//"0" 
+//" " space in quotation mark 
+// []
+// {}
+let name= [];
+
+if(name){
+        console.log("condition is true");
+    }
+else{
+        console.log("condition is false");
+    }
