b1748a54f9efd4581b8e828cd53884fe19fa106d
DeyRaj <feb17rajandey@gmail.com>
2020-03-07 16:00:43 -0800

    arguments done

--- undefined-vs-null.js
+++ undefined-vs-null.js
@@ -1,20 +1,20 @@
-// let name;
+let name;
 
-// function add (num1 , num2){
-//     console.log(num1 + num2);
-//     return
-// }
+function add (num1 , num2){
+    console.log(num1 + num2);
+    return                             //returned nothing
+}
 
-// function add (num1 , num2){
-//     console.log(num1 , num2)
-// }
+function add (num1 , num2){
+    console.log(num1 , num2)           // if we don't pass any parameter which is required 
+}
 
-// const identity = {name:"Rajan", phone:13445};
-// console.log(identity.home);
+const identity = {name:"Rajan", phone:13445};
+console.log(identity.home);            // wrong property 
 
-// let fun = undefined;
-// console.log(fun);
+let fun = undefined;                   // reserved word + defined it as a undefined 
+console.log(fun);
 
 
 let ages =[12, 13, 14];
-console.log(ages[11]);
+console.log(ages[11]);                  // searching for 11th indexed value 
